9dc96029ad3b5e196bb0ac1731cc061eff8ed350
Kumar Shanu <kumar.shanu2020@vitbhopal.ac.in>
2022-02-04 22:12:49 +0530

    Added some details using comments

--- import_export/index.js
+++ import_export/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PI, { doublePi, triplePi } from "./math";
+import PI,{doublePi, triplePi} from "./math";
+
+
+// In place of exporting the pi, double pi and other separately 
+// We can simple use
+// import * as pi from "./math";
+// And here pi will be an object and we can call all the three things be:
+// pi.default
+// pi.doublePi
+// pi.triplePi
 
 
 // Here below double pi is a funciton so we need to specify the ()
 ReactDOM.render(
-    <ul>
-        <li>{PI}</li>
-        <li>{doublePi()}</li>
-        <li>{triplePi()}</li>
-    </ul>,
-    document.getElementById("root")
+  <ul>
+    <li>{PI}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
+  </ul>,
+  document.getElementById("root")
 );
