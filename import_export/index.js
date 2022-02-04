1065f2f5fd7786ddf06c9456583e2e0195a9604c
Kumar Shanu <kumar.shanu2020@vitbhopal.ac.in>
2022-02-04 22:20:20 +0530

    done with challenge of chapter 404 ES6 import export

--- import_export/index.js
+++ import_export/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PI,{doublePi, triplePi} from "./math";
+import PI, { doublePi, triplePi } from "./math";
 
 
 // In place of exporting the pi, double pi and other separately 
@@ -10,14 +10,17 @@ import PI,{doublePi, triplePi} from "./math";
 // pi.default
 // pi.doublePi
 // pi.triplePi
+// It is always a good practice to use the one given about rather than using the wildcard 
+// given in comments.
 
 
 // Here below double pi is a funciton so we need to specify the ()
 ReactDOM.render(
-  <ul>
-    <li>{PI}</li>
-    <li>{doublePi()}</li>
-    <li>{triplePi()}</li>
-  </ul>,
-  document.getElementById("root")
+    <ul>
+        <li>{PI}</li>
+        <li>{doublePi()}</li>
+        <li>{triplePi()}</li>
+    </ul>,
+    document.getElementById("root")
 );
+``
